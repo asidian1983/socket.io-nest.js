b978b3a1ab8715dffabf8fe58b8654daa4534469
asidian1983 <asidian@gmail.com>
2023-12-15 01:06:28 +0900

    폴더 추가 및 파일명 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserController } from './controller/user.controller';
+import { UserService } from './service/user.service';
+import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [EventsModule],
+  controllers: [UserController],
+  providers: [UserService],
 })
 export class AppModule {}
